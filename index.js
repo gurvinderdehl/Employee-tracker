b4dca88137fdfeec9af679b45ae9e9dd48a649fe
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-03-11 21:25:20 +0000

    Added a connection to database

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
 require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'employees_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as ID ${connection.threadId}`);
+  
+    
+});
